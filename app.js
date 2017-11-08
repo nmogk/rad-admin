b6b70997cc690fcb8a0c1b7a7660bcc2d4eda4bd
Nathan Mogk <nm8911@gmail.com>
2017-11-08 12:51:21 -0700

    Added basic refs templating

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var refs = require('./routes/refs');
 
 var app = express();
 
@@ -16,8 +17,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/refs', refs);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
