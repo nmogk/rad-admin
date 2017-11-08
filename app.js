c1f614d7297da7ebd341d3ad62ab57b0432eb807
Nathan Mogk <nm8911@gmail.com>
2017-11-08 10:15:48 -0700

    Minor changes to match original code. Additional localizations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -11,17 +12,9 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.engine('hbs', hbs({
-  extname: 'hbs', 
-  defaultLayout: 'layout', 
-  layoutDir: __dirname + '/views',
-  partialsDir  : [
-      //  path to your partials
-      __dirname + '/views/partials',
-  ]
-}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
