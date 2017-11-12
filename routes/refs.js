f62c7c57c3025168e24811b662716b1b445b10e4
Nathan Mogk <nm8911@gmail.com>
2017-11-12 15:39:33 -0700

    Enabled display of username and users on reffs

--- routes/refs.js
+++ routes/refs.js
@@ -6,6 +6,8 @@ var fs = require("fs");
 router.get('/', function(req, res, next) {
     var contents = fs.readFileSync("database.json");
     var dbMeta = JSON.parse(contents);
+    dbMeta.username = req.user.get("name") || req.user.get("email");
+    dbMeta.users = req.user.get("permission") >= 2;
     res.render('refs', dbMeta);
 });
 
