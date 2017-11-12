a87ece1bd6e6ac2e093183041837e86e9bdf6227
Nathan Mogk <nm8911@gmail.com>
2017-11-12 16:12:57 -0700

    Added sensible profile page (no functionality yet)

--- routes/profile.js
+++ routes/profile.js
@@ -7,7 +7,12 @@ var router = express.Router();
 // we will want this protected so you have to be logged in to visit
 // we will use route middleware to verify this (the isLoggedIn function)
 router.get('/', function(req, res, next) {
-    res.render('profile', { user: req.user });
+    var replacements = {};
+    replacements.email = req.user.get("email");
+    replacements.dispname = req.user.get("name")
+    replacements.username = req.user.get("name") || req.user.get("email");
+    replacements.users = req.user.get("permission") >= 2;
+    res.render('profile', replacements);
 });
 
 module.exports = router;
--- routes/refs.js
+++ routes/refs.js
@@ -5,10 +5,10 @@ var fs = require("fs");
 /* GET home page. */
 router.get('/', function(req, res, next) {
     var contents = fs.readFileSync("database.json");
-    var dbMeta = JSON.parse(contents);
-    dbMeta.username = req.user.get("name") || req.user.get("email");
-    dbMeta.users = req.user.get("permission") >= 2;
-    res.render('refs', dbMeta);
+    var replacements = JSON.parse(contents);
+    replacements.username = req.user.get("name") || req.user.get("email");
+    replacements.users = req.user.get("permission") >= 2;
+    res.render('refs', replacements);
 });
 
 module.exports = router;
