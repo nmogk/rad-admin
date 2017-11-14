63f1283c348c6c2dc112a73ae18d026aacdee4ff
Nathan Mogk <nm8911@gmail.com>
2017-11-14 14:37:15 -0700

    Added email validation column to database

--- models/schema.js
+++ models/schema.js
@@ -4,7 +4,8 @@ var schema = {
         email: {type: 'string', maxlength: 254, nullable: false, unique: true},
         password_digest: {type: 'string', maxlength: 150, nullable: false},
         name: {type: 'string', maxlength: 150, nullable: true},
-        permission: {type: 'integer', unsigned: true, defaultTo: 0}
+        permission: {type: 'integer', unsigned: true, defaultTo: 0},
+        validated: {type: 'boolean', defaultTo: 0}
     },
     campaigns: {
         id: {type: 'increments', nullable: false, primary: true},
