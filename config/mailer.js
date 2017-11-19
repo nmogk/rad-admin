602c5ee97357646e2f7c4cac4b816480740eeadb
Nathan Mogk <nm8911@gmail.com>
2017-11-18 22:31:09 -0700

    Completion of  password reset functionality

--- config/mailer.js
+++ config/mailer.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 self.AWS = require('aws-sdk');
 
 self.AWS.config.update({
-    region: 'us-west-2'
+    region: 'us-east-1'
 });
 
 self.sesTransporter = nodemailer.createTransport({
@@ -15,7 +15,7 @@ self.sendResetMail = function(req, email, token){
     var mailOptions = {
         to: email,
         from: 'passwordreset@rad.creationeducation.org',
-        subject: 'Account Password Reset',
+        subject: 'RAD Admin Account Password Reset',
         text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
           'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
           'https://' + req.get('Host') + '/reset/' + token + '\n\n' +
@@ -28,13 +28,11 @@ self.sendPassChangeConfirmation = function(email){
     var mailOptions = {
         to: email,
         from: 'passwordreset@rad.creationeducation.org',
-        subject: 'Your password has been changed',
+        subject: 'Your RAD Admin password has been changed',
         text: 'Hello,\n\n' +
             'This is a confirmation that the password for your account ' + email + ' has just been changed.\n'
     };
-    self.sesTransporter.sendMail(mailOptions, function(err) {
-        req.flash('loginMessage', 'Success! Your password has been changed.');
-    });
+    return self.sesTransporter.sendMail(mailOptions);
 };
 
 module.exports = self;
--- routes/reset.js
+++ routes/reset.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var ResetToken = require('../models/invitations');
 var mail = require('../config/mailer');
 var validator       = require('../config/passValidator');
+var Promise = require('bluebird');
 
 router.get('/', function(req, res){
     res.redirect('/login');
@@ -18,36 +19,45 @@ router.get('/:token', function(req, res) {
     })
     .catch(function (err){
         req.flash('loginMessage', 'Password reset token is invalid or has expired.');
-        return res.redirect('/login');
+        res.redirect('/login');
     });
 });
 
 router.post('/:token', function(req, res) {
-    new ResetToken({token: req.params.token})
+    if(req.body.password !== req.body.confirm) {
+        req.flash('passChangeMessage', 'Passwords do not match.');
+        res.redirect(303, 'back');
+        return;
+    }
+    if(! validator.validate(req.body.password)) {
+        req.flash('passChangeMessage', 'Password is not strong enough. Passwords must have 8-72 characters and contain at least one numeral, uppercase, and lowercase letters.');
+        res.redirect(303, 'back');
+        return;
+    }
+
+    var tokenPromise = new ResetToken({token: req.params.token})
     .where('expires', '>', Date.now())
     .fetch({required: true, withRelated: 'user'})
-    .then(function (token) {
-        if(req.body.password !== req.body.confirm) {
-            req.flash('passChangeMessage', 'Passwords do not match.');
-            return res.redirect('/reset');
-        }
-        if(! validator.validate(req.body.password)) {
-            req.flash('passChangeMessage', 'Password is not strong enough. Passwords must have 8-72 characters and contain at least one numeral, uppercase, and lowercase letters.');
-            return res.redirect('/reset');
-        }
-        let user = token.related('user');
-        user.password = req.body.password;
-        user.save()
-        .then(function (user){
-            mail.sendPassChangeConfirmation(user.email);
-            req.flash('loginMessage', 'Success! Your password has been changed.');
-            res.redirect('/login');
-        });
-        token.destroy();
-    })
     .catch(function (err){
         req.flash('loginMessage', 'Password reset token is invalid or has expired.');
-        return res.redirect('/login');
+        res.redirect(303, '/login');
+        throw err;
+    });
+
+    var userPromise = tokenPromise.then(function (token) {
+        
+        let user = token.related('user');
+        user.set('password', req.body.password);
+        return user.save();
+    });
+
+    Promise.join(tokenPromise, userPromise, function(token, user){
+        mail.sendPassChangeConfirmation(user.get('email'));
+        req.flash('loginMessage', 'Success! Your password has been changed.');
+        token.destroy();
+    })
+    .finally(function (){
+        res.redirect(303, '/login');
     });
 
 });
