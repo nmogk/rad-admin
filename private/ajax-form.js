ce45e636dadc1cf961e8055ee152fca205e00792
Nathan Mogk <nm8911@gmail.com>
2018-05-22 16:39:14 -0700

    New object call

--- private/ajax-form.js
+++ private/ajax-form.js
@@ -1,15 +1,29 @@
-jQuery(function($) {
-    $('form[data-async]').on('submit', function(event) {
+jQuery(function ($) {
+    $('.modal').on('submit', 'form[data-async]', function (event) {
         var $form = $(this);
         var $target = $($form.attr('data-target'));
+        console.log("Form submission activated");
 
         $.ajax({
             type: $form.attr('method'),
             url: $form.attr('action'),
             data: $form.serialize(),
 
-            success: function(data, status) {
-                $target.html(data);
+            success: function (data, status) {
+                console.log("Submit success");
+                $.each(target.split("|"), function (i, val) {
+                    if (val == "close") {
+                        $form.closest(".modal").modal("hide");
+                    } else if (val == "event") {
+                        $form.trigger("ajax-submit");
+                    } else {
+                        $(val).html(data);
+                    }
+                });
+            }, 
+
+            error: function (jqXHR, status, error) {
+                console.log(status);
             }
         });
 
