896c9def41e15707516d335d93d1f7851a71a8e5
Nathan Mogk <nm8911@gmail.com>
2019-08-09 11:07:43 -0700

    Update solrconfig

--- config/solr-proxy.js
+++ config/solr-proxy.js
@@ -1,6 +1,6 @@
 var proxyOptions = {
     validHttpMethods: ['GET'],
-    validPaths: ['/solr/rad/refs', '/solr/rad/refs/csv', '/solr/source/select'],
+    validPaths: ['/solr/rad/refs', '/solr/rad/refs/csv', 'solr/rad/update', '/solr/source/select'],
     invalidParams: ['qt', 'stream'],
     backend: {
         host: 'localhost',
