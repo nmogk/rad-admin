3327059b3c0426e4d74c07e601d553abe663b8a6
nmogk <4602943+nmogk@users.noreply.github.com>
2026-04-13 07:45:53 -0700

    Integrate source name hinting and new source creation from reference edits #52 #53

--- private/refs.js
+++ private/refs.js
@@ -21,7 +21,10 @@ RefViewModel.prototype.deleteRef = function () {
  */
 RefViewModel.prototype.editRef = function () {
     formError('');
+    sourceSuggestions([]);
+    sourceNotFound(false);
     ko.cleanNode($("#editRefModal")[0]) // Must clear bindings in newer version of KO
+    this.source.subscribe(lookupSources);
     ko.applyBindings(this, $("#editRefModal")[0]);
     $("#editRefModal").modal({ backdrop: 'static' });
 }
@@ -29,6 +32,8 @@ RefViewModel.prototype.editRef = function () {
 RefViewModel.prototype.submitEdits = function () {
     var self = this;
     formError('');
+    sourceSuggestions([]);
+    sourceNotFound(false);
     self.commit();
     $.ajax({ // Makes an AJAX query to the server for the source
         url: "/refs/" + self.id(),
@@ -53,6 +58,8 @@ RefViewModel.prototype.newRefHandler = function () {
     var self = this;
     formError('');
     formSuccess('');
+    sourceSuggestions([]);
+    sourceNotFound(false);
     self.commit();
     $.ajax({
         url: "/refs/new",
@@ -84,6 +91,8 @@ RefViewModel.prototype.saveAndAddAnother = function () {
     var self = this;
     formError('');
     formSuccess('');
+    sourceSuggestions([]);
+    sourceNotFound(false);
     self.commit();
     $.ajax({
         url: "/refs/new",
@@ -122,6 +131,8 @@ function searchInit() {
     if (localStorage['refsEditor']) {
         blankRefViewModel.update(localStorage['refsEditor']);
     }
+    // Subscribe to source field changes for autocomplete
+    blankRefViewModel.source.subscribe(lookupSources);
     ko.applyBindings(blankRefViewModel, $("#newRefModal")[0]);
 
     if (queryString.q !== undefined) {
@@ -136,6 +147,106 @@ function searchInit() {
 // Shared observables for form validation errors and success messages
 var formError = ko.observable('');
 var formSuccess = ko.observable('');
+
+// Source autocomplete
+var sourceSuggestions = ko.observableArray([]);
+var sourceNotFound = ko.observable(false);
+var _sourceTimer = null;
+
+function lookupSources(value) {
+    if (!value || value.length < 2) {
+        sourceSuggestions([]);
+        sourceNotFound(false);
+        return;
+    }
+    if (_sourceTimer) { clearTimeout(_sourceTimer); }
+    _sourceTimer = setTimeout(function () {
+        $.ajax({
+            url: "/solr/source/select?",
+            dataType: "jsonp",
+            jsonp: "json.wrf",
+            data: $.param({ q: 'name:' + value + '*', rows: 8, fl: 'name' }),
+            success: function (data) {
+                var names = [];
+                data.response.docs.forEach(function (doc) {
+                    var n = doc.name;
+                    if (Array.isArray(n)) { n = n[0]; }
+                    if (n) { names.push(n); }
+                });
+                sourceSuggestions(names);
+                sourceNotFound(data.response.numFound === 0);
+            },
+            error: function () {
+                sourceSuggestions([]);
+                sourceNotFound(false);
+            }
+        });
+    }, 300);
+}
+
+function selectSource(name) {
+    // Find the active ref view model by checking which modal is visible
+    var modal = $('.modal.in');
+    if (modal.length) {
+        var ctx = ko.dataFor(modal.find('form')[0]);
+        if (ctx && ctx.source) {
+            ctx.source(name);
+        }
+    }
+    sourceSuggestions([]);
+    sourceNotFound(false);
+}
+
+function createSourceFromRef() {
+    // Find the active ref modal's source value
+    var modal = $('.modal.in');
+    var sourceName = '';
+    if (modal.length) {
+        var ctx = ko.dataFor(modal.find('form')[0]);
+        if (ctx && ctx.source) {
+            sourceName = ctx.source() || '';
+        }
+    }
+    // Hide the ref modal temporarily
+    modal.modal('hide');
+
+    // Create a blank source view model with the name pre-filled
+    var blankSource = new SrcViewModel({ name: sourceName });
+    ko.cleanNode($("#newSourceModal")[0]);
+
+    // Override the new source handler to return to the ref modal after save
+    var originalHandler = SrcViewModel.prototype.newSourceHandler;
+    blankSource.newSourceHandler = function () {
+        var self = this;
+        formError('');
+        formSuccess('');
+        self.commit();
+        $.ajax({
+            url: "/sources/new",
+            contentType: "application/json",
+            data: JSON.stringify(self.cache.latestData),
+            type: "POST",
+            success: function (data) {
+                $("#newSourceModal").modal("hide");
+                sourceNotFound(false);
+                sourceSuggestions([]);
+                // Re-show the ref modal
+                modal.modal({ backdrop: 'static' });
+            },
+            error: function (jqXHR) {
+                var msg = 'Error creating source';
+                if (jqXHR.responseJSON && jqXHR.responseJSON.error) {
+                    msg = jqXHR.responseJSON.error;
+                }
+                formError(msg);
+            }
+        });
+        return false;
+    };
+
+    ko.applyBindings(blankSource, $("#newSourceModal")[0]);
+    $("#newSourceModal").modal({ backdrop: 'static' });
+}
 
 // Make sure the whole page is loaded before manipulating it
 $(document).ready(searchInit());
